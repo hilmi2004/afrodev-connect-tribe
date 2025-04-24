863e2d590042c674b8fcfc4c023f865bbc3ef0e1
HP Recovery <gabihilmi4@gmail.com>
2025-04-24 13:55:54 +0100

    Deploy new build

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/afrodev-connect-tribe/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
